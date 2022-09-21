423feaf3cd429b58293160e130314a958b45b146
rizkialimaulana <maulanarizkiali@gmail.com>
2022-09-21 21:09:12 +0700

    Adding cors

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ import videoRoutes from './routes/Videos.js';
 import commentRoutes from './routes/Comments.js';
 import userRoutes from './routes/Users.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
-dotenv.config()
+dotenv.config();
+
 
 const connect = ()=> {
     mongoose.connect(process.env.MONGO).then(()=> {
@@ -18,6 +20,7 @@ const connect = ()=> {
     })
 }
 
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
